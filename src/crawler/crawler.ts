c96c13ea0a37a33c2aed374695ab58d938c78b91
Angelo Dias <angelo@foobar.agency>
2023-02-28 16:22:14 +0000

    Add serverless puppeteer

--- src/crawler/crawler.ts
+++ src/crawler/crawler.ts
@@ -1,4 +1,15 @@
-import puppeteer, { Browser, Page } from "puppeteer"
+import edgeChromium from "chrome-aws-lambda"
+
+// Importing Puppeteer core as default otherwise
+// it won't function correctly with "launch()"
+import puppeteer, { Browser, Page } from "puppeteer-core"
+
+// You may want to change this if you're developing
+// on a platform different from macOS.
+// See https://github.com/vercel/og-image for a more resilient
+// system-agnostic options for Puppeteeer.
+const LOCAL_CHROME_EXECUTABLE =
+  "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 
 export type Crawler = { browser: Browser; page: Page }
 
@@ -13,7 +24,14 @@ export type ErrorMessage = {
 
 export const crawler: CrawlerProps = {
   up: async () => {
-    const browser = await puppeteer.launch({ headless: true })
+    const executablePath =
+      (await edgeChromium.executablePath) || LOCAL_CHROME_EXECUTABLE
+
+    const browser = await puppeteer.launch({
+      executablePath,
+      args: edgeChromium.args,
+      headless: true,
+    })
     const page = await browser.newPage()
     await page.setViewport({ width: 1080, height: 1024 })
 
