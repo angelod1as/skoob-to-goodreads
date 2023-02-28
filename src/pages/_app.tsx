07ac4df8b0d3c213243adc9ba7ebaaac79e77fd7
Angelo Dias <angelo@foobar.agency>
2023-02-28 16:28:30 +0000

    Downgrading to allow serverless chromium

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,7 @@
 import { ToastContainer } from "react-toastify"
-import { Lato } from "@next/font/google"
 import "@/styles/globals.css"
 import "react-toastify/dist/ReactToastify.css"
 import type { AppProps } from "next/app"
-
-const lato = Lato({
-  weight: ["300", "400", "700"],
-  style: ["normal", "italic"],
-  subsets: ["latin"],
-})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -19,11 +12,6 @@ export default function App({ Component, pageProps }: AppProps) {
           background: "rgba(244, 241, 234, 1)", // Beige Header
         }}
       />
-      <style jsx global>{`
-        html {
-          font-family: ${lato.style.fontFamily};
-        }
-      `}</style>
       <Component {...pageProps} />
     </>
   )
